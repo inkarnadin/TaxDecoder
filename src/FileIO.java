80bce99b75fe9ca85dbaf7436be9a201e9812c05
inkarnadin <nasyrovvad@gmail.com>
2019-05-28 01:11:16 +0500

    Update FileIO.java

--- src/FileIO.java
+++ src/FileIO.java
@@ -2,6 +2,7 @@ package kurz.java.taxdecoder;
 
 import java.io.*;
 import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,18 +10,25 @@ public class FileIO
 {
     public static List<File> getFileList(String directoryName)
     {
+        List<File> result = new ArrayList<>();
         try
         {
-            return Files.walk(Paths.get(directoryName))
+            if (Files.notExists(Paths.get(directoryName)))
+            {
+                System.err.println("Директория не существует: " + directoryName);
+                return result;
+            }
+            
+            result = Files.walk(Paths.get(directoryName))
                     .filter(Files::isRegularFile)
                     .map(Path::toFile)
                     .collect(Collectors.toList());
         }
         catch (IOException e)
         {
-            System.err.println(e.getMessage());
-            return null;
+            System.err.println(e.getMessage());            
         }
+        return result;
     }
             
     public static void saveToCsv(List<ResultStore> list, String fileName)
